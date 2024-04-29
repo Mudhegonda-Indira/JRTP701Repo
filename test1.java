29d02e482741a7261ccb21d49758d01bfeba4718
Gandhi Mudhegonda <bunnymudhegonda@gmail.com>
2024-04-29 15:30:04 +0530

    dev1 committed code

--- test1.java
+++ test1.java
@@ -4,6 +4,9 @@ class test1
 	public void m1(){
 		System.out.println("m1");
 	}
+	public void m2(){
+		System.out.println("m2() method completed");
+	}
 	public static void main(String[] args){
 		System.out.println("Hello");
 		System.out.println("Hello Bunny bbb");
